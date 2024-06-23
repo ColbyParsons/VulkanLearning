03e5cee5a235b9ca05a8557be1e15ca0c5ed38f4
ColbyParsons <colbyparsons88@gmail.com>
2024-06-23 15:17:16 -0700

    Last addition before stopping today

--- main.cpp
+++ main.cpp
@@ -134,6 +134,8 @@ class HelloTriangleApplication {
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
 
+    std::vector<VkImageView> swapChainImageViews;
+
   public:
     void run() {
         initWindow();
@@ -545,6 +547,36 @@ class HelloTriangleApplication {
         vkGetSwapchainImagesKHR( device, swapChain, &imageCount, swapChainImages.data() );
     }
 
+    void createImageViews() {
+        swapChainImageViews.resize(swapChainImages.size());
+        for (size_t i = 0; i < swapChainImages.size(); i++) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if ( vkCreateImageView( device, &createInfo, nullptr, &swapChainImageViews[i] ) != VK_SUCCESS ) {
+                throw std::runtime_error( "Failed to create image views." );
+            }
+        }
+    }
+
+    void createGraphicsPipeline() {
+
+    }
+
     void initVulkan() {
         createInstance();
         setupDebugMessenger();
@@ -552,6 +584,8 @@ class HelloTriangleApplication {
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
+        createGraphicsPipeline();
     }
 
     void mainLoop() {
@@ -561,6 +595,9 @@ class HelloTriangleApplication {
     }
 
     void cleanup() {
+        for ( auto imageView : swapChainImageViews ) {
+            vkDestroyImageView( device, imageView, nullptr );
+        }
         vkDestroySwapchainKHR( device, swapChain, nullptr );
         vkDestroyDevice( device, nullptr );
         if ( enableValidationLayers ) {
